9724c3ae20582cd8a097991dff19ad4f5b431797
moriyuu <moriyu54321@gmail.com>
2019-01-28 16:03:40 +0900

    update: Don't Use JaveScript for Management State

--- README.md
+++ README.md
@@ -3,5 +3,8 @@
 > A journey to find the best accordion UI implementation
 > （最高のアコーディオン UI の実装を探す旅）
 
+## Features
+
 - Don't Use jQuery
-- Don't Use Fixed value max-height
+- Don't Use Fixed Value `max-height`
+- Don't Use JaveScript for Management State
--- index.tsx
+++ index.tsx
@@ -2,13 +2,41 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import styled from "styled-components";
 
-const Wrapper = styled.div<{ open: boolean }>`
+const items = [
+  {
+    id: 1,
+    head: "Accordion Title 1",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  },
+  {
+    id: 2,
+    head: "Accordion Title 2",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  },
+  {
+    id: 3,
+    head: "Accordion Title 3",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  },
+  {
+    id: 4,
+    head: "Accordion Title 4",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  }
+];
+
+const Wrapper = styled.div`
   margin-top: 20px;
   background: #fff;
   border-radius: 6px;
   border: 4px solid #ccc;
 
   > .head {
+    display: block;
     color: #326fa3;
     padding: 16px;
     font-weight: bold;
@@ -18,68 +46,38 @@ const Wrapper = styled.div<{ open: boolean }>`
   > .content {
     background: #cecece;
     color: #444;
-    line-height: ${props => (props.open ? 1.5 : 0)};
-    opacity: ${props => (props.open ? 1 : 0)};
-    padding: ${props => (props.open ? "20px" : "0 20px !important")};
-    transition: all 0.2s ease-out;
+    line-height: 0;
+    opacity: 0;
+    padding: 0 20px;
+    transition: all 0.25s ease-out;
+  }
+
+  > input[type="checkbox"] {
+    display: none;
+
+    &:checked ~ .content {
+      line-height: 1.5;
+      opacity: 1;
+      padding: 20px;
+    }
   }
 `;
 
-type Item = { id: number; head: string; content: string; open: boolean };
+type Item = { id: number; head: string; content: string };
 
-const Accordion = (props: Item & { toggle(item: Item["id"]): void }) => {
+const Accordion = (props: Item) => {
   return (
-    <Wrapper open={props.open}>
-      <div className="head" onClick={() => props.toggle(props.id)}>
+    <Wrapper>
+      <input type="checkbox" id={"checkbox_" + props.id} />
+      <label className="head" htmlFor={"checkbox_" + props.id}>
         {props.head}
-      </div>
+      </label>
       <div className="content">{props.content}</div>
     </Wrapper>
   );
 };
 
-class App extends React.Component<{}, { items: Item[] }> {
-  state = {
-    items: [
-      {
-        id: 1,
-        head: "Accordion Title 1",
-        content:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        open: false
-      },
-      {
-        id: 2,
-        head: "Accordion Title 2",
-        content:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        open: false
-      },
-      {
-        id: 3,
-        head: "Accordion Title 3",
-        content:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        open: false
-      },
-      {
-        id: 4,
-        head: "Accordion Title 4",
-        content:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        open: false
-      }
-    ]
-  };
-
-  toggle = (id: Item["id"]) => {
-    this.setState(_state => ({
-      items: _state.items.map(item =>
-        item.id === id ? { ...item, open: !item.open } : item
-      )
-    }));
-  };
-
+class App extends React.Component {
   render() {
     return (
       <div style={{ maxWidth: "800px", margin: "0 auto" }}>
@@ -95,34 +93,47 @@ class App extends React.Component<{}, { items: Item[] }> {
             ?
           </span>
         </h1>
-        {this.state.items.map(item => (
-          <Accordion key={item.id} {...item} toggle={this.toggle} />
+
+        {items.map(item => (
+          <Accordion key={item.id} {...item} />
         ))}
+
         <pre
           dangerouslySetInnerHTML={{
             __html: `
-            const Wrapper = styled.div<{ open: boolean }>\`
+            const Wrapper = styled.div\`
                 margin-top: 20px;
                 background: #fff;
                 border-radius: 6px;
                 border: 4px solid #ccc;
-
+            
                 > .head {
+                    display: block;
                     color: #326fa3;
                     padding: 16px;
                     font-weight: bold;
                     cursor: pointer;
                 }
-
+            
                 > .content {
                     background: #cecece;
                     color: #444;
-                    line-height: \${props => (props.open ? 1.5 : 0)};
-                    opacity: \${props => (props.open ? 1 : 0)};
-                    padding: \${props => props.open ? "20px" : "0 20px !important"};
+                    line-height: 0;
+                    opacity: 0;
+                    padding: 0 20px;
                     transition: all 0.2s ease-out;
                 }
-            \`;
+            
+                > input[type="checkbox"] {
+                    display: none;
+                
+                    &:checked ~ .content {
+                        line-height: 1.5;
+                        opacity: 1;
+                        padding: 20px;
+                    }
+                }
+          \`;
           `
           }}
         />
